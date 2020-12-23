6f5e6b3f987957e3b87edd5f75c77576e45acb71
Improbdrive <sam.wakelam@hotmail.com>
2020-12-23 13:13:00 +0000

    udating heroku

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,16 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 // create connection for mysql 
-const con = mysql.createConnection({
+// const con = mysql.createConnection({
+// 	host: process.env.HOST,
+// 	// cant call database on same port as the server
+// 	port: process.env.MYSQL,
+// 	user: process.env.USER,
+// 	password: process.env.PASSWORD,
+// 	database: process.env.DATABASE,
+// });
+
+const con = mysql.createPool({
 	host: process.env.HOST,
 	// cant call database on same port as the server
 	port: process.env.MYSQL,
